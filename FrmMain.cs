70f15cf5b48997a810de9e37467b673d4d607499
M.Thomas <m.thomas@gso.schule.koeln>
2023-03-13 10:28:50 +0100

    Uc hinzugefügt

--- FrmMain.cs
+++ FrmMain.cs
@@ -5,6 +5,19 @@ namespace WinFormsBlanko
         public FrmMain()
         {
             InitializeComponent();
+            ResizeRedraw = true;
         }
+
+        private void FrmMain_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int w = this.ClientSize.Width;
+            int h = this.ClientSize.Height;
+
+
+        }
+
+
     }
 }
